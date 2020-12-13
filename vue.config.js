91494a27d93d086fef28d61c649bf3a446c72c95
yui <ynakano1127@gmail.com>
2020-12-14 03:41:46 +0900

    [add]vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,17 @@
 module.exports = {
-    publicPath: '',
-    configureWebpack: {
-      output: {
-        filename: '[name].js',
-        chunkFilename: '[name].js'
-      }
-    },
-    css: {
-      extract: {
-        filename: '[name].css',
-      },
-    },
-    // productionSourceMap: process.env.NODE_ENV === 'production' ? false : true,
-    // chainWebpack: config => {
-    //     config.plugins.delete('preload')
-    // }
-  }
-  
+  "publicPath": "",
+  "configureWebpack": {
+    "output": {
+      "filename": "[name].js",
+      "chunkFilename": "[name].js"
+    }
+  },
+  "css": {
+    "extract": {
+      "filename": "[name].css"
+    }
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
